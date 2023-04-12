6ffe75055a904364727a146c37dd9d223d0ac9a1
Sven <sven@vioms.com>
2023-04-13 01:04:47 +0200

    Adding documentation to Model

--- src/Models/City.php
+++ src/Models/City.php
@@ -2,14 +2,31 @@
 
 namespace Vioms\Cities\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property integer $id Unique identifier
+ * @property string $name Name of the city
+ * @property string $ascii_name Name of the city in ascii
+ * @property array $alternate_names Alternative names for the cities
+ * @property float $latitude Latitude of the city
+ * @property float $longitude Longitude of the city
+ * @property string $feature_class feature_class of the city See: http://www.geonames.org/export/codes.html
+ * @property string $feature_code feature code of the city See: http://www.geonames.org/export/codes.html
+ * @property integer $population population of the city
+ * @property integer $elevation elevation of the city
+ * @property string $timezone in which timezone the city is located
+ * @property Carbon $updated_at last updated
+ */
 class City extends Model
 {
 
     protected $table;
 
     public $timestamps = false;
+
+    protected $dates = ['updated_at'];
 
     protected $casts = [
         'alternate_names' => 'json'
@@ -37,6 +54,12 @@ class City extends Model
         parent::__construct();
     }
 
+    /**
+     * Return an array that can be used to build a select element.
+     * @param $countryId
+     * @param $display
+     * @return array
+     */
     public function getSelectList($countryId, $display='ascii_name'): array
     {
         return $this->query()->where('country_id', $countryId)->get(['id', $display])->pluck($display, 'id')->toArray();
